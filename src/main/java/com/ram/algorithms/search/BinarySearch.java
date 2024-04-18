03ec0a41be7d47ad56335be1ff484a87757a4a31
Sai <chanty.cheers@gmail.com>
2024-04-17 22:13:26 -0400

    Binary search without recursion

--- src/main/java/com/ram/algorithms/search/BinarySearch.java
+++ src/main/java/com/ram/algorithms/search/BinarySearch.java
@@ -1,34 +1,29 @@
 package com.ram.algorithms.search;
-
-import java.sql.SQLOutput;
-import java.util.Arrays;
-
 public class BinarySearch {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5};
         int start = 0;
-        System.out.println(search(arr, 0, arr.length, 5));
+        System.out.println(search(arr, 0, arr.length, 6));
     }
 
-    public static boolean search(int[] arr, int start, int end, int val) {
-        int mid = (start + end) / 2;
-        boolean found = false;
+    public static int search(int[] arr, int start, int end, int val) {
+
         while (start < end) {
+            int mid = (start + end) / 2;
             if (val > arr[mid]) {
                 System.out.println("IN BLOCK 1");
-                return search(arr, mid + 1, end, val);
+                start = mid + 1;
             } else if (val < arr[mid]) {
                 System.out.println("IN BLOCK 2");
-                return search(arr, start, mid, val);
+                end = mid - 1;
             } else {
                 System.out.println("IN found block");
                 System.out.println("found " + val);
-                found = true;
                 System.out.println("start:" + start + ",end:" + end);
-                return true;
+                return mid;
             }
         }
-        return found;
+        return -1;
     }
 }
