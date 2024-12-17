366c169a618a7a3a4c9e9df19cb99851f1d42f18
Sai <chanty.cheers@gmail.com>
2024-12-17 12:23:14 -0500

    Fibonacci with cache

--- src/main/java/com/ram/project/Fibonacci.java
+++ src/main/java/com/ram/project/Fibonacci.java
@@ -4,33 +4,32 @@ import java.util.HashMap;
 
 public class Fibonacci {
 
-    static HashMap<Integer,Integer> results = new HashMap<>();
+    static HashMap<Long,Long> results = new HashMap<>();
     public static void main(String[] args) {
 
         if(args.length == 0) {
             throw new RuntimeException("Enter the parameter 1 to calculate the Fibonacci value");
         }
         try {
-            Integer num = Integer.parseInt(args[0]);
+            long num = Long.parseLong(args[0]);
             System.out.println(calculateFib(num));
         } catch(Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    public static Integer calculateFib(int num) {
+    public static long calculateFib(long num) {
         if(results.containsKey(num))  {
             return results.get(num);
         }
 
-        if(num == 0 || num == 1) {
+        if(num < 2) {
             return num;
         }
 
-
-        int x = calculateFib(num - 2);
+        long x = calculateFib(num - 2);
         results.put(num-2, x);
-        int y = calculateFib(num - 1 );
+        long y = calculateFib(num - 1 );
         results.put(num-1, y);
         return x + y;
     }
